32933e3c5c7ba29b6da47bd4363312cbd603fc9f
george <george@georges-MacBook-Pro-2.local>
2015-02-13 09:48:50 +1000

    starting point.... arrays working

--- drawOnBackground_Testing/AppDelegate.swift
+++ drawOnBackground_Testing/AppDelegate.swift
@@ -8,16 +8,86 @@
 
 import Cocoa
 
+let pi = M_PI
+
+
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
 
+    @IBOutlet weak var DrawingView: NSView!
+    
+     var resultantValueArray = [Double]()
+    
+    
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        
+        
+        var sineWave1 = SineWave()
+        var sineWave2 = SineWave()
+        var sineWave3 = SineWave()
+        sineWave1.calculateRawValues()
+        
+        
+//        for i in 0...359{
+//        println("the value at \(i) degrees is \(sineWave1.phaseAdjustedValueArray[i])")
+//        }
+        
+        
+        
+        func calculateResultantValues(){
+            for i in 0...359{
+                resultantValueArray[i] = sineWave1.phaseAdjustedValueArray[i] + sineWave2.phaseAdjustedValueArray[i] + sineWave3.phaseAdjustedValueArray[i]
+            }
+            
+            
+        }
+        
+        println("\(sineWave3.wavPhase) degrees")
+        println("\(sineWave3)")
+        
+        sineWave3.wavPhase = 33
+        
+          println("\(sineWave3.wavPhase) degrees")
+        
+// draw test line over graph paper image
+//        DrawingView.lockFocus()
+//        var path = NSBezierPath()
+//        path.lineWidth = 1.0
+//        path.moveToPoint( NSPoint(x:0,y:0))
+//        path.lineToPoint(( NSPoint(x:200,y:200)))
+//        path.stroke()
+        
+        DrawingView.lockFocus()
+        var path = NSBezierPath()
+        path.lineWidth = 1.0
+        path.moveToPoint( NSPoint(x:0,y:0))
+        for i in 0...30{
+            
+         path.lineToPoint( NSPoint(x:i,y:100))
+      
+        }
+        path.stroke()
+        
+
+        
+        
+        
     }
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
---
+++ drawOnBackground_Testing/SineWaveClass.swift
@@ -0,0 +1,101 @@
+//
+//  SineWaveClass.swift
+//  drawOnBackground_Testing
+//
+//  Created by george on 12/02/2015.
+//  Copyright (c) 2015 george. All rights reserved.
+//
+
+import Cocoa
+import Foundation
+
+class SineWave{
+
+    //Properties **************************
+    
+    var wavAmplitude:Double
+    var wavFrequency:Double
+    var wavPhase:Int
+    
+    var wavRawValueArray = [Double]()
+    var phaseAdjustedValueArray = [Double]()
+    
+    
+   
+
+    
+   //Methods ******************************
+    
+    init(){
+        wavAmplitude = 1
+        wavFrequency = 1000
+        wavPhase = 30
+        
+        for i in 0...360 {
+            self.phaseAdjustedValueArray.append(0)
+        }
+        
+        
+    } // end init
+    
+//*******************************
+    
+    func calculateRawValues ()  {
+     
+   
+      var instantaneousAmpValue :Double
+        
+        for i in 0...360 {
+            var degreeCount:Double = Double(i)
+            instantaneousAmpValue = Double(sin(degreeCount * pi / 180))
+            instantaneousAmpValue = instantaneousAmpValue * self.wavAmplitude
+            
+            self.wavRawValueArray.append(instantaneousAmpValue)
+            
+            }
+       self.adjustValuesForPhase()
+    }
+    
+//******************************
+    
+    func adjustValuesForPhase (){
+        
+        var startDegreeOffset:Int = 360 - self.wavPhase
+
+        for i in startDegreeOffset...360 {
+            
+            println("the index here is\(i)")
+           
+            println("the rv index here is\(i)")
+            println("the pa index here is\(i-startDegreeOffset)")
+            println("  ")
+            
+            self.phaseAdjustedValueArray[(i-startDegreeOffset)] = self.wavRawValueArray[i]
+        }
+        
+        for i in 1...(360 - self.wavPhase){
+            
+            println("the index here is\(i)")
+           
+            println("the rv index here is\(i-1)")
+            println("the pa index here is\(self.wavPhase + i)")
+            println("  ")
+            
+            self.phaseAdjustedValueArray[(self.wavPhase + i)] = self.wavRawValueArray[(i-1)]
+
+        }
+  
+        
+    }
+    
+    // NOTE  when a wave is 30 degrees out of phase, it is delayed by 30 degrees.. so now 330 aligns to zero degrees on the reference wave
+
+    
+    
+//*******************************
+    
+   
+  
+//********************************
+    
+} //end class
